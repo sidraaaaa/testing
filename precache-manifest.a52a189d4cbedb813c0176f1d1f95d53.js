463cb847a812f2e59e16f7f3a08f7b5805794976
sidraaaaa <sidra_rehmani1@hotmail.com>
2020-06-26 00:37:05 -0400

    Updates

---
+++ precache-manifest.a52a189d4cbedb813c0176f1d1f95d53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb8240475a5dea2514f990811cc0474",
+    "url": "/git.io/testing/index.html"
+  },
+  {
+    "revision": "74ba846c0a130bdaa133",
+    "url": "/git.io/testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0934c9feb45941bfbb98",
+    "url": "/git.io/testing/static/js/2.50cca996.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git.io/testing/static/js/2.50cca996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ba846c0a130bdaa133",
+    "url": "/git.io/testing/static/js/main.487f21a5.chunk.js"
+  },
+  {
+    "revision": "a606e6283504d28068cb",
+    "url": "/git.io/testing/static/js/runtime-main.540c81ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git.io/testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
