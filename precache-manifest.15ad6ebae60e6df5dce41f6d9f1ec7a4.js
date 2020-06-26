6c26ec1a35791e9b827df23d52a1d9e17c757998
sidraaaaa <sidra_rehmani1@hotmail.com>
2020-06-26 00:48:20 -0400

    Updates

---
+++ precache-manifest.15ad6ebae60e6df5dce41f6d9f1ec7a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2cee97d1267808c168e833b6618e76",
+    "url": "/github.io/testing/index.html"
+  },
+  {
+    "revision": "d739b771a74699eb7bcf",
+    "url": "/github.io/testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0934c9feb45941bfbb98",
+    "url": "/github.io/testing/static/js/2.50cca996.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/testing/static/js/2.50cca996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d739b771a74699eb7bcf",
+    "url": "/github.io/testing/static/js/main.c2679372.chunk.js"
+  },
+  {
+    "revision": "ecf5d860f29683727e95",
+    "url": "/github.io/testing/static/js/runtime-main.5f42da27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/git.io/testing/precache-manifest.a52a189d4cbedb813c0176f1d1f95d53.js"
+  "/github.io/testing/precache-manifest.15ad6ebae60e6df5dce41f6d9f1ec7a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git.io/testing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/testing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
